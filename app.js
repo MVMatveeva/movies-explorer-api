eca60ab53015adac6211ae2afcb066c164357405
Maria Matveeva <marikadenisova@yandex.ru>
2024-03-19 13:49:28 +0300

    api with CORS

--- app.js
+++ app.js
@@ -12,14 +12,21 @@ const { PORT, MONGO_URL } = require('./utils/utils');
 
 const app = express();
 
-const allowedCors = [
-  'https://matveeva.movie.nomoredomainswork.ru',
-  'http://matveeva.movie.nomoredomainswork.ru',
-  'http://localhost:3000',
-  'http://localhost:3001',
-  'https://localhost:3000',
-  'https://localhost:3001',
-];
+const allowedCors = {
+  origin: [
+    'https://matveeva.movie.nomoredomainswork.ru',
+    'http://matveeva.movie.nomoredomainswork.ru',
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'https://localhost:3000',
+    'https://localhost:3001',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
 
 app.use(cors(allowedCors));
 
