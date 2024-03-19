73d26295d11c0eb1366e9798e5bf41a8da0589b1
Maria Matveeva <marikadenisova@yandex.ru>
2024-03-19 22:24:25 +0300

    api errors

--- app.js
+++ app.js
@@ -6,27 +6,20 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const { errors } = require('celebrate');
 const router = require('./routes');
-//const { handleErrorCentralized } = require('./middlewares/handleErrorCentralized');
+const { handleErrorCentralized } = require('./middlewares/handleErrorCentralized');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT, MONGO_URL } = require('./utils/utils');
 
 const app = express();
 
-const allowedCors = {
-  origin: [
-    'https://matveeva.movie.nomoredomainswork.ru',
-    'http://matveeva.movie.nomoredomainswork.ru',
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'https://localhost:3000',
-    'https://localhost:3001',
-  ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-  credentials: true,
-};
+const allowedCors = [
+  'https://matveeva.movie.nomoredomainswork.ru',
+  'http://matveeva.movie.nomoredomainswork.ru',
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'https://localhost:3000',
+  'https://localhost:3001',
+];
 
 app.use(cors(allowedCors));
 
